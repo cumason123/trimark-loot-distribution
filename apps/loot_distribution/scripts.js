ebcf37a067962aff60a79d418f3602734e6801a1
Curtis Mason <cumason@bu.edu>
2020-09-07 04:57:28 -0400

    added commas

--- apps/loot_distribution/scripts.js
+++ apps/loot_distribution/scripts.js
@@ -1,45 +1,45 @@
 let number_of_modules = 0;
 let number_of_members = 0;
-let hash = 0
-let module_ids = {}
-let member_ids = {}
+let hash = 0;
+let module_ids = {};
+let member_ids = {};
 
 function random(items) {
-  return items[Math.floor(Math.random()*items.length)];
+  return items[Math.floor(Math.random() * items.length)];
 }
 
 function deleteModule(hash) {
   number_of_modules -= 1;
   for (key in module_ids[hash]) {
-    const node = document.getElementById(module_ids[hash][key])
-    node.remove()
+    const node = document.getElementById(module_ids[hash][key]);
+    node.remove();
   }
-  delete module_ids[hash]
+  delete module_ids[hash];
 }
 
 function deleteMember(hash) {
   number_of_members -= 1;
   for (key in member_ids[hash]) {
-    const node = document.getElementById(member_ids[hash][key])
-    node.remove()
+    const node = document.getElementById(member_ids[hash][key]);
+    node.remove();
   }
-  delete member_ids[hash]
+  delete member_ids[hash];
 }
 
 function addModule() {
-  hash = parseInt(hash) + 1
+  hash = parseInt(hash) + 1;
 
   const itemsContainer = document.getElementById("modules");
-  const child = document.createElement("div")
+  const child = document.createElement("div");
   const ids = {
     quantity: `quantity_${hash}`,
     cost: `cost_${hash}`,
     module: `module_${hash}`,
-    deleteButton: `delete_module_${hash}`
-  }
-  module_ids[hash] = ids
+    deleteButton: `delete_module_${hash}`,
+  };
+  module_ids[hash] = ids;
 
-  child.innerHTML= `
+  child.innerHTML = `
         <div>
             <input
             placeholder="e.g. Corpum C-Type Medium Laser"
@@ -59,7 +59,7 @@ function addModule() {
             id="${ids.cost}"
             placeholder="cost" 
             class="numeric" 
-            type="number" 
+            type="text" 
             />
 
             <button
@@ -68,20 +68,20 @@ function addModule() {
             onclick="deleteModule(${hash})">delete</button>
         </div>
     `;
-    itemsContainer.appendChild(child)
-    number_of_modules += 1;
+  itemsContainer.appendChild(child);
+  number_of_modules += 1;
 }
 
 function addMember() {
-  hash = parseInt(hash) + 1
+  hash = parseInt(hash) + 1;
 
   const membersContainer = document.getElementById("members");
-  const child = document.createElement("div")
+  const child = document.createElement("div");
   const ids = {
     member: `member_${hash}`,
-    deleteButton: `delete_member_${hash}`
-  }
-  member_ids[hash] = ids
+    deleteButton: `delete_member_${hash}`,
+  };
+  member_ids[hash] = ids;
   child.innerHTML = `
     <div>
       <input
@@ -96,65 +96,72 @@ function addMember() {
       class="deleteButton"
       onclick="deleteMember(${hash})">delete</button>
     </div>
-  `
-  membersContainer.appendChild(child)
+  `;
+  membersContainer.appendChild(child);
   number_of_members += 1;
 }
 
 function give_to(member, module_name, cost) {
   if (module_name in member) {
-    member[module_name] += 1
+    member[module_name] += 1;
   } else {
-    member[module_name] = 1
+    member[module_name] = 1;
   }
-  member.loot_value += cost
+  member.loot_value += cost;
 }
 
 function calculate_distribution() {
   let loot = [];
   let modules = [];
   for (hash in module_ids) {
-    const module_id = module_ids[hash]
-    console.log("README", hash, module_ids, module_id)
-    const module_name = document.getElementById(module_id.module).value
-    const quantity = parseInt(document.getElementById(module_id.quantity).value)
-    const cost = parseFloat(document.getElementById(module_id.cost).value)
-    modules.push({module_name, quantity, cost})
+    const module_id = module_ids[hash];
+    console.log("README", hash, module_ids, module_id);
+    const module_name = document.getElementById(module_id.module).value;
+    const quantity = parseInt(
+      document.getElementById(module_id.quantity).value
+    );
+    const cost = parseFloat(
+      document.getElementById(module_id.cost).value.replaceAll(",", "")
+    );
+
+    modules.push({ module_name, quantity, cost });
   }
-  modules.sort((module1, module2) => module1.cost > module2.cost ? -1 : 1)
+  modules.sort((module1, module2) => (module1.cost > module2.cost ? -1 : 1));
   // Generate loot
   for (hash in member_ids) {
-    const member_id = member_ids[hash]
-    const member_name = document.getElementById(member_id.member).value
-    loot.push({name: member_name, loot_value: 0})
+    const member_id = member_ids[hash];
+    const member_name = document.getElementById(member_id.member).value;
+    loot.push({ name: member_name, loot_value: 0 });
   }
 
   for (let i = 0; i < modules.length; i++) {
-    const {module_name, cost, quantity} = modules[i]
+    const { module_name, cost, quantity } = modules[i];
     for (let k = 0; k < quantity; k++) {
-      loot.sort((member1, member2) => member1.loot_value > member2.loot_value ? 1 : -1)
-      const min_loot_val = loot[0].loot_value
+      loot.sort((member1, member2) =>
+        member1.loot_value > member2.loot_value ? 1 : -1
+      );
+      const min_loot_val = loot[0].loot_value;
 
       // Find smallest loot values
-      let eligible_users = []
+      let eligible_users = [];
       for (let j = 0; j < loot.length; j++) {
-        const member = loot[j]
+        const member = loot[j];
         if (member.loot_value != min_loot_val) {
-          break
+          break;
         }
-        eligible_users.push(j)
+        eligible_users.push(j);
       }
-      let randomly_chosen_member = loot[random(eligible_users)]
-      give_to(randomly_chosen_member, module_name, cost)
+      let randomly_chosen_member = loot[random(eligible_users)];
+      give_to(randomly_chosen_member, module_name, cost);
     }
   }
 
-  let organized_loot = {}
+  let organized_loot = {};
   for (let i = 0; i < loot.length; i++) {
-    const name = loot[i].name
-    organized_loot[name] = loot[i]
-    delete organized_loot[name].name
+    const name = loot[i].name;
+    organized_loot[name] = loot[i];
+    delete organized_loot[name].name;
   }
-  const result = document.getElementById("result")
-  result.innerHTML = JSON.stringify(organized_loot, null, 4)
+  const result = document.getElementById("result");
+  result.innerHTML = JSON.stringify(organized_loot, null, 4);
 }
